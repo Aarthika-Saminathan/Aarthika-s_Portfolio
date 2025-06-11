5882a2c21740577eb8e7b892acdbcf1e1a226883
aarthika51 <aarthikasaminathan7896@gmail.com>
2025-06-11 15:47:25 +0530

    Update navbar and remove underline effect

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -12,55 +12,55 @@ import powerbicert from '../assets/certificates/power_bi_cer.jpeg';
 
 const Services_Data = [
   {
-    s_no: "01",
+   
     s_name: "AWS (S3 & Cloud Storage)",
     s_desc: "Understanding AWS fundamentals, especially S3 bucket setup, permissions, and cloud storage management.",
     certificate: awsCert
   },
   {
-    s_no: "02",
+  
     s_name: "C++",
     s_desc: "Proficient in C++ programming including object-oriented principles and data structures.",
     certificate: cppCert
   },
   {
-    s_no: "03",
+   
     s_name: "Foundation of Web Development",
     s_desc: "Strong foundation in HTML, CSS, JavaScript, and React for building modern, responsive web applications.",
     certificate: backendCert
   },
   {
-    s_no: "04",
+
     s_name: "Front-End Development",
     s_desc: "Developing responsive and user-friendly interfaces using HTML, CSS, and modern design principles.",
     certificate: algoCert
   },
   {
-    s_no: "05",
+  
     s_name: "React JS Development",
     s_desc: "Creating dynamic and component-driven web applications using React JS and state management tools.",
     certificate: dbCert
   },
   {
-    s_no: "06",
+    
     s_name: "JavaScript",
     s_desc: "Completed JavaScript training covering concepts like DOM manipulation, ES6+, and event handling.",
     certificate: gitCert
   },
   {
-    s_no: "07",
+ 
     s_name: "Internship Experience",
     s_desc: "Worked on real-time web projects during internship at Zeckho IT Services.",
     certificate: internCert
   },
   {
-    s_no: "08",
+  
     s_name: "Python Programming",
     s_desc: "Completed Python course from IIT Certification Program.",
     certificate: pythonCert
   },
   {
-    s_no: "09",
+ 
     s_name: "Power BI",
     s_desc: "Completed training in Power BI for data visualization and business intelligence.",
     certificate: powerbicert
